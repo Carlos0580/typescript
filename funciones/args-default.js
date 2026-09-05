0005ea51b7aa3bcb0e7ef69cfce82dda4f7f30c9
Carlos J Sanchez G <carlosjesus0580@gmail.com>
2026-09-05 12:20:41 -0400

    parametros por defecto

---
+++ funciones/args-default.js
@@ -0,0 +1,14 @@
+"use strict";
+(() => {
+    const fullName = (firstName, lastName, upper) => {
+        if (upper) {
+            return `${firstName} ${lastName || '---'}`.toUpperCase();
+        }
+        else {
+            return `${firstName} ${lastName || '---'}`;
+        }
+    };
+    const name = fullName('Tony', 'Stark');
+    console.log({ name });
+})();
+//# sourceMappingURL=args-default.js.map
---
+++ funciones/args-rest.js
@@ -0,0 +1,4 @@
+"use strict";
+(() => {
+})();
+//# sourceMappingURL=args-rest.js.map
